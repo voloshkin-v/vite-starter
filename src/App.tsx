44cce9661abf47f9d34ede3200803ce812341afa
Vitalii <voloshin.v.02@gmail.com>
2024-03-22 10:07:18 +0100

    setup test starter app

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+export const App = () => {
+    return (
+        <div>
+            <h1>Test h1</h1>
+            <h2>Test h2</h2>
+            <button className="btn">Button</button>
+        </div>
+    );
+};
---
+++ src/sum.test.tsx
@@ -0,0 +1,3 @@
+test('myLogic', () => {
+    expect(1).toBe(1);
+});
